6bbdb4013fbfd4bb8dcef0199fa9b0719c9ea09e
ryuenu <ryuenuse@gmail.com>
2015-05-01 20:07:08 +0900

    paging + community모아보기

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(function(req, res, next){
 app.use('/', routes);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var ObjectId = require('mongodb').ObjectId;
 
 var router = express.Router();
+var LIMIT = 50;
 
 /* GET home page. */
 router.get('/list', function (req, res, next) {
     var page = req.query.page;
-
+    var community = req.query.community;
     if (page === undefined || page === '') {
         page = 1;
     }
@@ -31,19 +32,37 @@ router.get('/view', function (req, res, next) {
 router.get('/article/list', function (req, res, next) {
     var db = req.db;
     var page = req.query.page;
+    var community = req.query.community;
+    var where = { "contents": { "$exists": true } };
 
     if (page === undefined || page === '') {
         page = 1;
     }
-    page = (page - 1) * 50
+    if (community !== undefined) {
+        where.community = community;
+    }
+    page = (page - 1) * LIMIT;
 
-    db.collection('article').find({ contents: { $exists: true } }, { contents: false })
-        .sort({ date: -1 }).skip(page).limit(50).toArray(
+    db.collection('article').find(where, { "contents": false })
+        .sort({ date: -1 }).skip(page).limit(LIMIT).toArray(
         function (err, article_list) {
             if (err) {
                 throw err;
             }
-            res.json(article_list);
+
+            var result = {};
+            db.collection('article').find(where).count(function (err, cnt) {
+                if (err) {
+                    throw err;
+                    //next(err);
+                }
+
+                result.totalcount = cnt;
+                result.limits = LIMIT;
+                result.datas = article_list;
+                res.json(result);
+            });
+
         }
     );
 });
