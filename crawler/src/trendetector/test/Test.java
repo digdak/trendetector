8b29d40eb865fea9cf9eec19f21d7dd600769c39
baleian <baleian90@gmail.com>
2015-05-19 04:44:13 +0900

    통계컬렉션 추가 및 OU ListCrawler NumberFormat에러 처리 + 매우큰본문내용 처리 추가

--- crawler/src/trendetector/test/Test.java
+++ crawler/src/trendetector/test/Test.java
@@ -1,17 +1,24 @@
 package trendetector.test;
 
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import trendetector.KeywordExtractionEngine;
+import org.jsoup.nodes.Document;
+
+import trendetector.crawler.FileURL;
 
 
 public class Test {
 	
 	public static void main(String[] args) {
 		try {
-//			String url = "http://www.dogdrip.net/dogdrip";
-//			Document doc = new Document(url);
+			String url = "http://www.dogdrip.net/72607284";
+			Document doc = new Document(url);
+			doc.html(FileURL.getHtml(url));
+			String strDate = doc.select(".dateAndCount .date").text();
+			SimpleDateFormat strToDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+			Date date = strToDateFormat.parse(strDate);
+			System.out.println(date.getTime());
 //			doc.html("<body><div><font>  1  </font></div></body>");
 ////			Elements items = doc.select(".boardList tbody tr");
 //
@@ -24,9 +31,15 @@ public class Test {
 //			System.out.println(Jsoup.clean(str, url, Whitelist.none().addAttributes("img", "src", "style")));
 //			
 			
-			String contents = "<div>안녕<br>하세요</div>";
+//			String contents = "<div>안녕<br>하세요</div>";
 //			contents = contents.replaceAll("<", " <").replace(">", "> ");
 			
+//			SimpleDateFormat dateToStrFormat = new SimpleDateFormat("yy/MM/dd");
+//			SimpleDateFormat strToDateFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+//			Date now = new Date();
+//			Date date = strToDateFormat.parse(dateToStrFormat.format(now) + " " + "23:03:00");
+//			System.out.println(date.getTime());
+//			System.out.println(now.getTime());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
--- data/scripts/full-reset.js
+++ data/scripts/full-reset.js
@@ -67,3 +67,90 @@ db.article.ensureIndex(
 	{ board_id: 1, article_no: 1 },
 	{ unique: true }
 );
+
+db.statistics.drop();
+db.statistics.insert({ _id: 0, totalcnt: 0 });
+
+db.statistics.keywords.drop();
+
+db.system.js.remove({});
+// ======== 1주일 전 데이터들 통계 누적 =========
+db.system.js.save({
+	_id: "removeAndStatisticsBeforeWeek",
+	value: function () {
+		if (db.statistics_temp.count() != 0) {
+			return { ok: false, error: "already exists statistics_temp." };
+		}
+
+		var map = function () {
+			this.keywords.forEach(function (data) {
+				var key = data.keyword;
+				if (key.length < 2 || key.length > 15) {
+					return;
+				}
+
+				emit(key, 1);
+			});
+		};
+
+		var reduce = function (key, values) {
+			var count = 0;
+			values.forEach(function (cnt) {
+				count += cnt;
+			});
+			return count;
+		};
+
+		var now = new ISODate();
+		now.setDate(now.getDate() - 7);
+
+		var result = 
+		db.article.mapReduce(map, reduce, {
+			out: "statistics_temp",
+			query: { 
+				keywords: { $exists: true, $not: { $eq: false } },
+				date: { $lt : now }
+			}
+		});
+
+		if (result.ok != 1) {
+			db.statistics_temp.drop();
+			return { ok: false, result: result };
+		}
+		var inc_cnt = result.counts.input;
+		if (inc_cnt == 0) {
+			return { ok: true, result: "no data" };
+		}
+
+		db.article.remove({ date: { $lt: now } });
+
+		var result = db.statistics.update(
+				{ _id: 0 }, 
+				{ $inc: { totalcnt: inc_cnt } },
+				{ upsert: true }
+			);
+
+		if (result.nMatched == 0 
+		&& result.nUpserted == 0 
+		&& result.nModified == 0) {
+			return { ok: false, result: result };
+		}
+
+		var bulkUpdate = db.statistics.keywords.initializeUnorderedBulkOp();
+		db.statistics_temp.find().forEach(function (doc) {
+			bulkUpdate.find({_id: doc._id}).upsert().update(
+				{ $inc: { cnt: doc.value } }
+			);
+		});
+		var result = bulkUpdate.execute();
+
+		if (result.nMatched == 0 
+		&& result.nUpserted == 0 
+		&& result.nModified == 0) {
+			return { ok: false, result: result };
+		}
+
+		db.statistics_temp.drop();
+	}
+});
+// ====================================
