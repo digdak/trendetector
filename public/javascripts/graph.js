d57a0256874d0ae85907a6af6762c3721b4de339
Shin JJang <makao.rule@gmail.com>
2015-05-23 15:35:56 +0900

    graph db와 연결하여 graph render(neo4j 7474 port에 id: neo4j pw:1234로 설정해야됨)

---
+++ public/javascripts/graph.js
@@ -0,0 +1,83 @@
+function createRandomGraph() {	
+	var i,
+    s,
+    N = 100,
+    E = 500,
+    g = {
+      nodes: [],
+      edges: []
+    };
+	// Generate a random graph:
+	for (i = 0; i < N; i++)
+	  g.nodes.push({
+	    id: 'n' + i,
+	    label: 'Node ' + i,
+	    x: Math.random(),
+	    y: Math.random(),
+	    size: Math.random(),
+	    color: '#666'
+	  });
+	for (i = 0; i < E; i++)
+	  g.edges.push({
+	    id: 'e' + i,
+	    source: 'n' + (Math.random() * N | 0),
+	    target: 'n' + (Math.random() * N | 0),
+	    size: Math.random(),
+	    color: '#ccc'
+	  });
+	// Instantiate sigma:
+	s = new sigma({
+	  graph: g,
+	  container: 'graph-container'
+	});
+	console.log("create called");
+}
+
+function createGraph(nodes, relations) {
+	var i,
+    s,
+    N = nodes.length,
+    E = relations.length,
+    g = {
+      nodes: [],
+      edges: []
+    };
+	// Generate a random graph:
+	for (i = 0; i < N; i++)
+	  g.nodes.push({
+	    id: nodes[i]._node._data.data.id,
+	    label: nodes[i]._node._data.data.id,
+	    x: Math.random()%50,
+	    y: Math.random()%50,
+	    size: 10,
+	    color: '#666'
+	  });
+	console.log(g);
+	for (i = 0; i < E; i++)
+	  g.edges.push({
+	    id: 'e' + i,
+	    source: relations[i][0],
+	    target: relations[i][1],
+	    size: Math.random(),
+	    color: '#ccc'
+	  });
+	console.log(g);
+	// Instantiate sigma:
+	s = new sigma({
+	  graph: g,
+	  container: 'graph-container'
+	});
+	console.log("create graph called");
+}
+
+function getGraph() {
+	$.getJSON('/graph/nodes_with_rel.json', function (data) {
+		console.log("in getGraph");
+		console.log(data);
+		nodes = data.keywords;
+		relations = data.relations;
+		createGraph(nodes, relations);
+	});
+}
+
+getGraph();
--- routes/controller.js
+++ routes/controller.js
@@ -8,6 +8,77 @@ var model_article = require('../model/article.js');
 var router = express.Router();
 var LIMIT = 50;
 
+var Keyword = require('../models/keyword.js');
+
+router.get('/graph/nodes', function (req, res, next) {
+    Keyword.getAll(function (err, keywords) {        
+        if (err) return next(err);
+        // res.json(keywords);
+        res.render('keywords', {'keywords': keywords});
+    });
+});
+
+router.get('/graph/nodes_with_rel/:term', function (req, res, next) {
+    var term = req.params.term;
+    console.log("routing /graph/nodes_with_rel/" + term);
+    // Check is there graph on time 
+    // if not make graph,
+    Keyword.getAll(term, function (err, keywords) {        
+        if (err) {
+            console.log(err);
+            return next(err);
+        }
+        
+        if (keywords.length != 0) {
+            // get orgin graph from db
+            Keyword.getPairs(function (err, results) {        
+
+                console.log("get pair");
+                if (err) return next(err);
+                res.json({keywords:keywords,relations:results});            
+            });
+        } else {
+            console.log("create keyword graph");
+            res.redirect('/graph/create/'+term);
+        }
+            
+    });    
+});
+
+// make new graph
+router.get('/graph/create/:term', function(req, res, next) {    
+    var term = req.params.term;
+    console.log("routing /graph/create/" + term);
+    model_keyword.get_keywords(Keyword.create_nodes(Keyword.create_graph(function(term) {
+        console.log("do redirect");
+        res.redirect('/graph/nodes_with_rel/'+term+'.json');
+    })))(req.db, term); // end of get keywords
+});
+
+router.get('/graph/relations', function (req, res, next) {
+    Keyword.getPairs(function (err, results) {        
+        if (err) return next(err);
+        res.json(results);
+        // res.render('keywords', {'keywords': keywords});
+    });
+});
+
+router.get('/graph/nodes/:id', function (req, res, next) {
+    var node_id = req.params.id;
+    Keyword.get(node_id, function (err, keyword) {                
+        if (err) return next(err);
+        // res.json(keyword);
+        keyword.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);            
+            res.render('keyword', {
+                keyword: keyword,
+                following: following,
+                others: others
+            });
+        });
+        
+    });
+});
 
 /* GET home page. */
 router.get('/list', function (req, res, next) {
@@ -31,38 +102,41 @@ router.get('/view', function (req, res, next) {
     res.render('view', { title: article_id, page: page, article_id: article_id });
 });
 
-router.get('/article/list', function (req, res, next) {
+router.post('/article/list', function (req, res, next) {
     var page = req.query.page;
-    var community = req.query.community;
-    var keyword = req.query.keyword;
+    var keyword = req.body.keyword;
+    var community = req.body["community[]"];
 
     if (page === undefined || page === '') {
         page = 1;
     }
     page = (page - 1) * LIMIT;
 
-    model_keyword.get_keyword_info(function (data) {
-        var where = { 'contents': { '$exists': true } };
+    var where = {
+        'contents': { '$exists': true },
+        'keywords': { '$exists': true }
+    };
 
-        if (data !== undefined) {
-            where._id = {'$in': data.article};
+    if (keyword !== undefined) {
+        where["keywords.keyword"] = keyword;
+    }
+
+    if (community !== undefined && community.length > 0) {
+        if (!Array.isArray(community)) {
+            community = [ community ];
         }
+        where.community = {'$in': community};
+    }
 
-        if (community !== undefined) {
-            where.community = community;
-        }
-
-        model_article.get_articlelist(function (article_list, total_cnt) {
-            var result = {};
-            result.totalcount = total_cnt;
-            result.limits = LIMIT;
-            result.datas = article_list;
-            res.json(result);
-        })(req.db, page, LIMIT, where);
-
-    })(req.db, keyword);
-
+    model_article.get_articlelist(function (article_list, total_cnt) {
+        var result = {};
+        result.totalcount = total_cnt;
+        result.limits = LIMIT;
+        result.datas = article_list;
+        res.json(result);
+    })(req.db, page, LIMIT, where);
 });
+
 
 
 router.get('/article/:id', function (req, res, next) {
@@ -73,10 +147,25 @@ router.get('/article/:id', function (req, res, next) {
     })(req.db, article_id);
 });
 
+
 router.get('/keyword/list', function (req, res, next) {
-    model_keyword.get_keywordlist_by_community(function (data) {
-        res.json(data);
-    })(req.db);
+    var term = req.query.term;
+/*
+    if (term == undefined) {
+        var err = new Error('Not Valid');
+        err.status = 403;
+        return next(err);
+    }
+*/
+    model_keyword.get_keywords(function (db, term, batch_time, keyword_list) {
+        var result = {};
+        result.batch_time = batch_time;
+        result.keywords = keyword_list;     
+        console.log(keyword_list);   
+        res.json(result);
+
+    })(req.db, term);
+
 });
 
 router.get('/community/list', function (req, res) {
@@ -89,4 +178,6 @@ router.get('/community/list', function (req, res) {
         }
     );
 });
+
+
 module.exports = router;
