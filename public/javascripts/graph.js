5eeda1d4d18e466e69ee4b33e1e35ea7c4a25edb
Shin JJang <makao.rule@gmail.com>
2015-06-06 02:07:50 +0900

    apply Force-directed graph

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -33,67 +33,87 @@ function createRandomGraph() {
 }
 
 function highlightConnected(s) {		
-      // We first need to save the original colors of our
-      // nodes and edges, like this:
-      s.graph.nodes().forEach(function(n) {
-        n.originalColor = n.color;
-      });
-      s.graph.edges().forEach(function(e) {
-        e.originalColor = e.color;
-      });
+  	// We first need to save the original colors of our
+	// nodes and edges, like this:
+	s.graph.nodes().forEach(function(n) {
+		n.originalColor = n.color;
+	});
+	s.graph.edges().forEach(function(e) {
+		e.originalColor = e.color;
+	});
 
-      // When a node is clicked, we check for each node
-      // if it is a neighbor of the clicked one. If not,
-      // we set its color as grey, and else, it takes its
-      // original color.
-      // We do the same for the edges, and we only keep
-      // edges that have both extremities colored.
-      s.bind('clickNode', function(e) {      	
+	// When a node is clicked, we check for each node
+	// if it is a neighbor of the clicked one. If not,
+	// we set its color as grey, and else, it takes its
+	// original color.
+	// We do the same for the edges, and we only keep
+	// edges that have both extremities colored.
+	s.bind('clickNode', function(e) {      	
+		var nodeId = e.data.node.id,
+		    toKeep = s.graph.neighbors(nodeId);
+		toKeep[nodeId] = e.data.node;
 
-        var nodeId = e.data.node.id,
-            toKeep = s.graph.neighbors(nodeId);
-        toKeep[nodeId] = e.data.node;
-		
-        get_article_with_keyword(nodeId);
+		get_article_with_keyword(nodeId);
 
-        s.graph.nodes().forEach(function(n) {
-          if (toKeep[n.id])
-            n.color = "#44B8FF";
-          else
-            n.color = '#eee';
-        });
+		s.graph.nodes().forEach(function(n) {
+			if (toKeep[n.id])
+				n.color = "#44B8FF";
+			else
+				n.color = '#eee';
+		});
 
-        // set color node clicked
-        e.data.node.color = '#ec5148';
+		// set color node clicked
+		e.data.node.color = '#ec5148';
 
-        s.graph.edges().forEach(function(e) {
-          if (toKeep[e.source] && toKeep[e.target])
-            e.color = "#ec5148";
-          else
-            e.color = '#eee';
-        });
+		s.graph.edges().forEach(function(e) {
+			if (toKeep[e.source] && toKeep[e.target])
+				e.color = "#ec5148";
+			else
+				e.color = '#eee';
+		});
 
-        // Since the data has been modified, we need to
-        // call the refresh method to make the colors
-        // update effective.
-        s.refresh();
-      });
+		s.stopForceAtlas2();
+		// Since the data has been modified, we need to
+		// call the refresh method to make the colors
+		// update effective.
+		s.refresh();
+	});
 
-      // When the stage is clicked, we just color each
-      // node and edge with its original color.
-      s.bind('clickStage', function(e) {      	
-        s.graph.nodes().forEach(function(n) {
-          n.color = n.originalColor;
-        });
+	// When the stage is clicked, we just color each
+	// node and edge with its original color.
+	s.bind('clickStage', function(e) {      	
+		s.graph.nodes().forEach(function(n) {
+		  n.color = n.originalColor;
+		});
 
-        s.graph.edges().forEach(function(e) {
-          e.color = e.originalColor;
-        });
+		s.graph.edges().forEach(function(e) {
+		  e.color = e.originalColor;
+		});
 
-        // Same as in the previous event:
-        s.refresh();
-      });
-    }
+		// Same as in the previous event:
+		s.refresh();
+	});
+}
+
+function force_power(sigInst) {
+	config = {};
+	config.barnesHutOptimize = false;
+	config.strongGravityMode = true;	
+	sigInst.startForceAtlas2(config);
+ 
+	var isRunning = true;
+	document.getElementById('stop-layout').addEventListener('click',function(){
+	    if(isRunning){
+        	isRunning = false;
+      		sigInst.stopForceAtlas2();
+	      	document.getElementById('stop-layout').childNodes[0].nodeValue = 'Start Layout';
+	    } else {
+	      	isRunning = true;
+	      	sigInst.startForceAtlas2();
+	      	document.getElementById('stop-layout').childNodes[0].nodeValue = 'Stop Layout';
+	    }
+    }, true);
+}	
 
 function createGraph(nodes, relations) {
 	var i,
@@ -109,9 +129,9 @@ function createGraph(nodes, relations) {
 	  g.nodes.push({
 	    id: nodes[i]._node._data.data.id,
 	    label: nodes[i]._node._data.data.id,
-	    x: Math.random()%50,
-	    y: Math.random()%50,
-	    size: 10,
+	    x: Math.random(),
+	    y: Math.random(),
+	    size: 1,
 	    color: '#666'
 	  });	
 	for (i = 0; i < E; i++)
@@ -140,6 +160,7 @@ function createGraph(nodes, relations) {
 	  container: 'graph-container'
 	});
 	highlightConnected(s);
+	force_power(s);
 	console.log("create graph called");
 }
 
